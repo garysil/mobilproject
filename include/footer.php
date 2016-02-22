d01c4f055e0599a58a749711e406d65c166bdf8e
garysil <gergely.siladi@akademiasovy.sk>
2016-02-22 09:47:22 +0100

    inserted intro text

--- include/footer.php
+++ include/footer.php
@@ -1,8 +1,9 @@
+
 <footer class="footer">
     
     <div class="containerfooter">
         <img src="img/copyright.png" width="13px" height="13px">
-        Made by: Gergely Siladi, Matej Pliešovský, Tomáš Sádovský
+        Made by: Gergely Siladi, Matej Plie&#353;ovsk&#253;, Tom&#225;&#353; S&#225;dovsk&#253;
         
     </div>
     
--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 <html>
 
 <head>
-    <meta charset=utf-8">
+    <meta charset="utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Kosice</title>
     <meta name="viewport" content="width = device-width, initial-scale = 1">
@@ -18,7 +19,22 @@
     
     <?php include('include/header.php'); ?>
     <main class="mainpart">
-    
+    <div class="introfirst">
+		Welcome to our site about Kosice. As the second biggest city in Slovakia , 
+		it has a population of a quarter million people. In many ways it is the centre 
+		of Eastern Slovakia. It has a very rich cultural life, it hosts many cultural 
+		events. In 2013 the city was a European Capital of Culture. Kosice has also a 
+		very active sports side. It has a successful handball and hockey team. 
+	</div>
+	<div class="introsecond">
+		Why did we create this page? As we didn´t find an interesting website about our 
+		city, we decided to create one ourselves. We wanted people from the whole world 
+		to be acquainted with Kosice. We want to inform people, who don´t know where to 
+		go through the Holidays, to consider visiting our city. We also a provide a playful 
+		quiz game about the history and points of interests of the city. Besides that we 
+		handpicked the pictures that give you an insight to the city´s life and style. 
+		We hope you´ll like our take on Kosice and you will visit it in the future.
+	</div>
     </main>
     <?php include('include/footer.php'); ?>
 </body>
