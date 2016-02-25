1bef544579b837a054d5223763f4011b5f941ce5
MatejPliesovsky <matej.pliesovsky@akademiasovy.sk>
2016-02-25 08:53:48 +0100

    rename

--- gallery.php
+++ gallery.php
@@ -18,21 +18,21 @@
 <body>
     <?php include('include/header.php'); ?>
     <main class="mainpart">
-        <img id="DSC_1302" src="img/buildings/1/DSC_1302.JPG" alt="Dom sv.Alzbety" width="50%">
-        <img id="DSC_1299" src="img/buildings/2/DSC_1299.JPG" alt="Urbanova veza" width="50%">
-		<img id="DSC_1307" src="img/buildings/3/DSC_1307.JPG" alt="Divadlo J.Borodaca" width="50%">
-		<img id="DSC_1309" src="img/buildings/4/DSC_1309.JPG" alt="Socha Imaculata" width="50%">
-		<img id="DSC_1313" src="img/buildings/5/DSC_1313.JPG" alt="Vychodoslovenske prirodovedne muzeum" width="50%">
-		<img id="DSC_1320" src="img/buildings/6/DSC_1320.JPG" alt="Marathon runner" width="50%">
-		<img id="DSC_1322" src="img/buildings/7/DSC_1322.JPG" alt="Budova ustavneho sudu" width="50%">
-		<img id="DSC_1324" src="img/buildings/8/DSC_1324.JPG" alt="DOminikansky kostol" width="50%">
-		<img id="DSC_1330" src="img/buildings/9/DSC_1330.JPG" alt="Zidovska synagoga Puskinova" width="50%">
-		<img id="DSC_1333" src="img/buildings/10/DSC_1333.JPG" alt="Zidovska synagoga Tajovskeho" width="50%">
-		<img id="DSC_1335" src="img/buildings/11/DSC_1335.JPG" alt="Dom umenia Moyzesova" width="50%">
-		<img id="DSC_1340" src="img/buildings/12/DSC_1340.JPG" alt="Steel Arena" width="50%">
-		<img id="DSC_1343" src="img/buildings/13/DSC_1343.JPG" alt="Miklusova väznica" width="50%">
-		<img id="DSC_1344" src="img/buildings/14/DSC_1344.JPG" alt="Jacabs palace" width="50%">
-		<img id="DSC_1301" src="img/buildings/15/DSC_1350.JPG" alt="Vyhliadkova veza" width="50%">
+        <img id="DSC_1302" src="img/buildings/1/1.JPG" alt="Dom sv.Alzbety" width="50%">
+        <img id="DSC_1299" src="img/buildings/2/1.JPG" alt="Urbanova veza" width="50%">
+		<img id="DSC_1307" src="img/buildings/3/1.JPG" alt="Divadlo J.Borodaca" width="50%">
+		<img id="DSC_1309" src="img/buildings/4/1.JPG" alt="Socha Imaculata" width="50%">
+		<img id="DSC_1313" src="img/buildings/5/1.JPG" alt="Vychodoslovenske prirodovedne muzeum" width="50%">
+		<img id="DSC_1320" src="img/buildings/6/1.JPG" alt="Marathon runner" width="50%">
+		<img id="DSC_1322" src="img/buildings/7/1.JPG" alt="Budova ustavneho sudu" width="50%">
+		<img id="DSC_1324" src="img/buildings/8/1.JPG" alt="DOminikansky kostol" width="50%">
+		<img id="DSC_1330" src="img/buildings/9/1.JPG" alt="Zidovska synagoga Puskinova" width="50%">
+		<img id="DSC_1333" src="img/buildings/10/1.JPG" alt="Zidovska synagoga Tajovskeho" width="50%">
+		<img id="DSC_1335" src="img/buildings/11/1.JPG" alt="Dom umenia Moyzesova" width="50%">
+		<img id="DSC_1340" src="img/buildings/12/1.JPG" alt="Steel Arena" width="50%">
+		<img id="DSC_1343" src="img/buildings/13/1.JPG" alt="Miklusova väznica" width="50%">
+		<img id="DSC_1344" src="img/buildings/14/1.JPG" alt="Jacabs palace" width="50%">
+		<img id="DSC_1301" src="img/buildings/15/1.JPG" alt="Vyhliadkova veza" width="50%">
     </main>
     <?php include('include/footer.php'); ?>
 </body>
