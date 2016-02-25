ad8eb3208955ac96e1b14e5499eadffc58f4dfa6
MatejPliesovsky <matej.pliesovsky@akademiasovy.sk>
2016-02-25 22:25:18 +0100

    popup edit

--- gallery.php
+++ gallery.php
@@ -14,27 +14,6 @@
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/popup.css">
 	
-	<script type="text/javascript">
-window.document.onkeydown = function (e)
-{
-	if (!e){
-		e = event;
-	}
-	if (e.keyCode == 27){
-		lightbox_close();
-	}
-}
-function lightbox_open(){
-	window.scrollTo(0,0);
-	document.getElementById('light').style.display='block';
-	document.getElementById('fade').style.display='block';	
-}
-function lightbox_close(){
-	document.getElementById('light').style.display='none';
-	document.getElementById('fade').style.display='none';
-}
-</script>
-
 </head>
 
 <body>
@@ -47,7 +26,7 @@ function lightbox_close(){
 	<a class="button" href="#popup1"><a class="imgContainer" href="#popup1"><img class="galleryPics" src="img/buildings/1/1.JPG" alt="St. Elisabeth Cathedral"></a></a>
 		<div id="popup1" class="pic">
 			<div class="popup">
-				<a class="imgContainer" href="#popup1"><img src="img/buildings/1/1.JPG" alt="St. Elisabeth Cathedral">
+				<a class="imgPop" href="#popup1"><img src="img/buildings/1/1.JPG" alt="St. Elisabeth Cathedral">
 				<a class="close" href="#">&times;</a>
 					<div class="content">
 					</div>
@@ -58,7 +37,7 @@ function lightbox_close(){
 	<a class="button" href="#popup2"><a class="imgContainer" href="#popup2"><img class="galleryPics" src="img/buildings/2/1.JPG" alt="St. Urban Tower"></a></a>
 		<div id="popup2" class="pic">
 			<div class="popup">
-				<a class="imgContainer" href="#popup2"><img src="img/buildings/2/1.JPG" alt="St. Urban Tower">
+				<a class="imgPop" href="#popup2"><img src="img/buildings/2/1.JPG" alt="St. Urban Tower">
 				<a class="close" href="#">&times;</a>
 					<div class="content">
 					</div>
@@ -69,7 +48,7 @@ function lightbox_close(){
 	<a class="button" href="#popup3"><a class="imgContainer" href="#popup3"><img class="galleryPics" src="img/buildings/3/1.JPG" alt="State Theatre Košice"></a></a>
 		<div id="popup3" class="pic">
 			<div class="popup">
-				<a class="imgContainer" href="#popup3"><img src="img/buildings/3/1.JPG" alt="State Theatre Košice">
+				<a class="imgPop" href="#popup3"><img src="img/buildings/3/1.JPG" alt="State Theatre Košice">
 				<a class="close" href="#">&times;</a>
 					<div class="content">
 					</div>
@@ -80,7 +59,7 @@ function lightbox_close(){
 	<a class="button" href="#popup4"><a class="imgContainer" href="#popup4"><img class="galleryPics" src="img/buildings/4/1.JPG" alt="Immaculata"></a></a>
 		<div id="popup4" class="pic">
 			<div class="popup">
-				<a class="imgContainer" href="#popup4"><img src="img/buildings/4/1.JPG" alt="Immaculata">
+				<a class="imgPop" href="#popup4"><img src="img/buildings/4/1.JPG" alt="Immaculata">
 				<a class="close" href="#">&times;</a>
 					<div class="content">
 					</div>
@@ -91,7 +70,7 @@ function lightbox_close(){
 	<a class="button" href="#popup5"><a class="imgContainer" href="#popup5"><img class="galleryPics" src="img/buildings/5/1.JPG" alt="East Slovak Museum"></a></a>
 		<div id="popup5" class="pic">
 			<div class="popup">
-				<a class="imgContainer" href="#popup5"><img src="img/buildings/5/1.JPG" alt="East Slovak Museum">
+				<a class="imgPop" href="#popup5"><img src="img/buildings/5/1.JPG" alt="East Slovak Museum">
 				<a class="close" href="#">&times;</a>
 					<div class="content">
 					</div>
@@ -102,7 +81,7 @@ function lightbox_close(){
 	<a class="button" href="#popup6"><a class="imgContainer" href="#popup6"><img class="galleryPics" src="img/buildings/6/1.JPG" alt="Statue of The Runner"></a></a>
 		<div id="popup6" class="pic">
 			<div class="popup">
-				<a class="imgContainer" href="#popup6"><img src="img/buildings/6/1.JPG" alt="Statue of The Runner">
+				<a class="imgPop" href="#popup6"><img src="img/buildings/6/1.JPG" alt="Statue of The Runner">
 				<a class="close" href="#">&times;</a>
 					<div class="content">
 					</div>
@@ -113,7 +92,7 @@ function lightbox_close(){
 	<a class="button" href="#popup7"><a class="imgContainer" href="#popup7"><img class="galleryPics" src="img/buildings/7/1.JPG" alt="Constitutional Court of Slovakia"></a></a>
 		<div id="popup7" class="pic">
 			<div class="popup">
-				<a class="imgContainer" href="#popup7"><img src="img/buildings/7/1.JPG" alt="Constitutional Court of Slovakia">
+				<a class="imgPop" href="#popup7"><img src="img/buildings/7/1.JPG" alt="Constitutional Court of Slovakia">
 				<a class="close" href="#">&times;</a>
 					<div class="content">
 					</div>
@@ -124,7 +103,7 @@ function lightbox_close(){
 	<a class="button" href="#popup8"><a class="imgContainer" href="#popup8"><img class="galleryPics" src="img/buildings/8/1.JPG" alt="Dominican church"></a></a>
 		<div id="popup8" class="pic">
 			<div class="popup">
-				<a class="imgContainer" href="#popup8"><img src="img/buildings/8/1.JPG" alt="Dominican church">
+				<a class="imgPop" href="#popup8"><img src="img/buildings/8/1.JPG" alt="Dominican church">
 				<a class="close" href="#">&times;</a>
 					<div class="content">
 					</div>
@@ -135,7 +114,7 @@ function lightbox_close(){
 	<a class="button" href="#popup9"><a class="imgContainer" href="#popup9"><img class="galleryPics" src="img/buildings/9/1.JPG" alt="Synagogue on Puškinova street"></a></a>
 		<div id="popup9" class="pic">
 			<div class="popup">
-				<a class="imgContainer" href="#popup9"><img src="img/buildings/9/1.JPG" alt="Synagogue on Puškinova street">
+				<a class="imgPop" href="#popup9"><img src="img/buildings/9/1.JPG" alt="Synagogue on Puškinova street">
 				<a class="close" href="#">&times;</a>
 					<div class="content">
 					</div>
@@ -146,7 +125,7 @@ function lightbox_close(){
 	<a class="button" href="#popup10"><a class="imgContainer" href="#popup10"><img class="galleryPics" src="img/buildings/10/1.JPG" alt="Neological synagogue"></a></a>
 		<div id="popup10" class="pic">
 			<div class="popup">
-				<a class="imgContainer" href="#popup10"><img src="img/buildings/10/1.JPG" alt="Neological synagogue">
+				<a class="imgPop" href="#popup10"><img src="img/buildings/10/1.JPG" alt="Neological synagogue">
 				<a class="close" href="#">&times;</a>
 					<div class="content">
 					</div>
@@ -157,7 +136,7 @@ function lightbox_close(){
 	<a class="button" href="#popup1"><a class="imgContainer" href="#popup11"><img class="galleryPics" src="img/buildings/11/1.JPG" alt="Steel Arena"></a></a>
 		<div id="popup11" class="pic">
 			<div class="popup">
-				<a class="imgContainer" href="#popup11"><img src="img/buildings/11/1.JPG" alt="Steel Arena">
+				<a class="imgPop" href="#popup11"><img src="img/buildings/11/1.JPG" alt="Steel Arena">
 				<a class="close" href="#">&times;</a>
 					<div class="content">
 					</div>
@@ -168,7 +147,7 @@ function lightbox_close(){
 	<a class="button" href="#popup12"><a class="imgContainer" href="#popup12"><img class="galleryPics" src="img/buildings/12/1.JPG" alt="Kosice State Philharmonic"></a></a>
 		<div id="popup12" class="pic">
 			<div class="popup">
-				<a class="imgContainer" href="#popup12"><img src="img/buildings/12/1.JPG" alt="Kosice State Philharmonic">
+				<a class="imgPop" href="#popup12"><img src="img/buildings/12/1.JPG" alt="Kosice State Philharmonic">
 				<a class="close" href="#">&times;</a>
 					<div class="content">
 					</div>
@@ -179,7 +158,7 @@ function lightbox_close(){
 	<a class="button" href="#popup13"><a class="imgContainer" href="#popup13"><img class="galleryPics" src="img/buildings/13/1.JPG" alt="Mikluš Prison"></a></a>
 		<div id="popup13" class="pic">
 			<div class="popup">
-				<a class="imgContainer" href="#popup13"><img src="img/buildings/13/1.JPG" alt="Mikluš Prison">
+				<a class="imgPop" href="#popup13"><img src="img/buildings/13/1.JPG" alt="Mikluš Prison">
 				<a class="close" href="#">&times;</a>
 					<div class="content">
 					</div>
@@ -190,7 +169,7 @@ function lightbox_close(){
 	<a class="button" href="#popup14"><a class="imgContainer" href="#popup14"><img class="galleryPics" src="img/buildings/14/1.JPG" alt="Jakab´s Palace"></a></a>
 		<div id="popup14" class="pic">
 			<div class="popup">
-				<a class="imgContainer" href="#popup14"><img src="img/buildings/14/1.JPG" alt="Jakab´s Palace">
+				<a class="imgPop" href="#popup14"><img src="img/buildings/14/1.JPG" alt="Jakab´s Palace">
 				<a class="close" href="#">&times;</a>
 					<div class="content">
 					</div>
@@ -201,7 +180,7 @@ function lightbox_close(){
 	<a class="button" href="#popup15"><a class="imgContainer" href="#popup15"><img class="galleryPics" src="img/buildings/15/1.JPG" alt="Watchtower"></a></a>
 		<div id="popup15" class="pic">
 			<div class="popup">
-				<a class="imgContainer" href="#popup15"><img src="img/buildings/15/1.JPG" alt="Watchtower">
+				<a class="imgPop" href="#popup15"><img src="img/buildings/15/1.JPG" alt="Watchtower">
 				<a class="close" href="#">&times;</a>
 					<div class="content">
 					</div>
